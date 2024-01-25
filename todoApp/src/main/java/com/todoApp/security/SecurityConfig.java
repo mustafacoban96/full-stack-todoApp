18cda85dad6eee0fb4b4bce7a4fdc6e6df0ecc8e
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-01-25 16:40:58 +0300

    customerDetailsService inject, AuthManager and Password encoder

--- todoApp/src/main/java/com/todoApp/security/SecurityConfig.java
+++ todoApp/src/main/java/com/todoApp/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.todoApp.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,7 +21,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 	
+	private CustomerDetailsService customerDetailsService;
 	
+	
+	
+	@Autowired
+	public SecurityConfig(CustomerDetailsService customerDetailsService) {
+
+		this.customerDetailsService = customerDetailsService;
+	}
+
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
@@ -47,5 +62,16 @@ public class SecurityConfig {
 				.build();
 		
 		return new InMemoryUserDetailsManager(admin,user);
+	}
+	
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
